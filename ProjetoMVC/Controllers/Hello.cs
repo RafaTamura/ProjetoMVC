c663ba465d8b9a2c140f213b29bb578a2201b5c8
Rafa Tamura <rafaelatamura@gmail.com>
2023-06-05 23:48:53 -0300

    Add project files.

---
+++ ProjetoMVC/Controllers/Hello.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ProjetoMVC.Controllers
+{
+    public class Hello : Controller
+    {
+        public IActionResult Index()
+        //public string Index()
+        {
+            return View();
+            //return "Apresenta essa mensagem";
+        }
+        public IActionResult Welcome(string nome, int numTimes = 10)
+        {
+            ViewData["Message"] = "Oi" + nome;
+            ViewData["NumTimes"] = numTimes;
+            //return HtmlEncoder.Default.Encode($"Oi {nome}, ID: {ID}");
+            return View();
+        }
+        public string Teste()
+        {
+            return "Outro";
+        }
+    }
+}
